7c560e142e9fcfeed7e62614cb0000f6d9c5f3fe
rsoemardja <rsoemardja@gmail.com>
2019-10-27 03:29:02 +1300

    Update Day0.java

---
+++ 30DaysofCode/Day 0 Hello, World/Java/Day0.java
@@ -0,0 +1,17 @@
+/*
+*/
+package helloworld;
+
+/**
+ * 
+ * @author RamaSoemardja
+ */
+public class HelloWorld {
+
+
+    public static void main(String[] args) {
+            // TODO code application here
+            System.out.println("Hello World");
+
+    }
+}
