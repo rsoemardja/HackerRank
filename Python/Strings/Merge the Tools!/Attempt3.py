1fcf3fbd4f215cccbd63b4aeac1526677b087542
rsoemardja <rsoemardja@gmail.com>
2020-01-13 23:12:29 +1300

    HackerRank Python
    Fixed Merge The Tools Attempt 3

--- Python/Strings/Merge the Tools!/Attempt3.py
+++ Python/Strings/Merge the Tools!/Attempt3.py
@@ -1,6 +1,6 @@
 def merge_the_tools(string, k):
     s = [string[(i+k):((i+l)*k)] for i in range(len(string)//k)]
-    print('\n'.join([''.join([x for i, x in enumerate(sx) if sx.in dex(x) == i]) for sx in s]))
+    print('\n'.join([''.join([x for i, x in enumerate(sx) if sx.index(x) == i]) for sx in s]))
 
 if __name__ == '__main__':
     string, k = raw_input(), int(raw_input())
