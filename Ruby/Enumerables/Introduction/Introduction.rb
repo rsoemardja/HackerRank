c87417eaf0237fffc788955eacd0ba4ae6c3b803
rsoemardja <rsoemardja@gmail.com>
2020-02-06 11:02:02 +1300

    HackerRank Ruby
    Ruby Enumerables Challenge on HackerRank

---
+++ Ruby/Enumerables/Introduction/Introduction.rb
@@ -0,0 +1,9 @@
+def iterate_colors(colors)
+	# Your code here
+	color_array = Array.new()
+	colors.each do |color|
+		color_array << color
+	end
+
+	return color_array
+end
---
+++ Ruby/Enumerables/collect/collect.rb
@@ -0,0 +1,10 @@
+def rot13(secret_messages)
+	# your code here
+
+	secret_messages.collect do |message|
+		message.tr!("abcdefghijklmnopqrstuvwxyz",
+		"nopqrstuvwxyzabcdefghijklm")
+	end
+
+	return secret_messages
+end
