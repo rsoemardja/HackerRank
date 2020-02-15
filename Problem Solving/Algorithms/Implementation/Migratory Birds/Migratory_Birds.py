df00dc4ea204425c6bbc676224806e27224efd27
rsoemardja <rsoemardja@gmail.com>
2020-02-15 18:42:14 +1300

    Problem Solving: Algorithms
    Solving with Solutions with Python

--- Problem Solving/Algorithms/Implementation/Migratory Birds/Migratory_Birds.py
+++ Problem Solving/Algorithms/Implementation/Migratory Birds/Migratory_Birds.py
@@ -8,10 +8,10 @@ import sys
 
 # Complete the migratoryBirds function below.
 def migratoryBirds(arr):
-    bird_freq = [0, 0, 0, 0, 0, 0, 0]
+    bird_freq = [0, 0, 0, 0, 0, 0]
     for i in range(len(arr)):
         bird_freq[arr[i]] += 1
-        return bird_freq.index(max(bird_freq))
+    return bird_freq.index(max(bird_freq))
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
