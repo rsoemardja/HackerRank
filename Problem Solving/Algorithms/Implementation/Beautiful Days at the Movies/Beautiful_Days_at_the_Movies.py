55e02210bbd6aa2849758858d40ac0c0c97ce24a
rsoemardja <rsoemardja@gmail.com>
2020-02-16 18:59:38 +1300

    HackerRank Problem Solving
    Solutions With Python

--- Problem Solving/Algorithms/Implementation/Beautiful Days at the Movies/Beautiful_Days_at_the_Movies.py
+++ Problem Solving/Algorithms/Implementation/Beautiful Days at the Movies/Beautiful_Days_at_the_Movies.py
@@ -8,11 +8,11 @@ import sys
 from decimal import *
 
 # Complete the beautifulDays function below.
-def beautiful(i, j, k):
+def beautifulDays(i, j, k):
     count=0
     for l in range(i,j+1):
         n = (str(l)[::-1])
-        if(abs(l-int(n))% k==0):
+        if(abs(l-int(n))%k==0):
             count+=1
     return count
 
--- Problem Solving/Algorithms/Implementation/Utopian Tree/Utopian_Tree.py
+++ Problem Solving/Algorithms/Implementation/Utopian Tree/Utopian_Tree.py
@@ -11,11 +11,11 @@ def utopianTree(n):
     boolean = False
     count = 0
     for i in range(n+1):
-        if(boolean == false):
+        if(boolean == False):
             count += 1
             boolean = True
         else:
-            count += 2
+            count *= 2
             boolean = False
     return count
 
